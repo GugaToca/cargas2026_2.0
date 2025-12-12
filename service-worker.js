bba6588274cc7be456fedca5cd625a5e6de3b3e7
GugaToca <gustavotocalino08@outlook.com>
2025-12-12 15:54:07 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "diario-cargas-v4"; // 👈 MUDE A VERSÃO SEMPRE QUE ATUALIZAR
+const CACHE_NAME = "diario-cargas-v5"; // 👈 MUDE A VERSÃO SEMPRE QUE ATUALIZAR
 
 const FILES_TO_CACHE = [
   "./",
